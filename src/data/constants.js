8b9fd47ea39a83d1146b19364daab06c55de999c
Gonzlzq <ggggggorn@gmail.com>
2024-10-26 15:31:17 +0700

    Portfolio commit
    Portfolio commit

---
+++ src/data/constants.js
@@ -0,0 +1,193 @@
+
+export const Bio = {
+  name: "Wongsakorn",
+  roles: [
+    "Full Stack Developer",
+    "Wep Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am an Electronics Engineering (Computer) student with a strong interest in studying website design. I am currently seeking an internship to apply my technical skills in real-world projects and further develop my abilities in these areas.",
+  github:   "https://github.com/ltzGon",
+  resume:   "https://drive.google.com/file/d/1yVhbBk4dnJDjPqRI9So2CrOZQzyVYRz-/view?usp=drive_link",
+  insta:    "https://www.instagram.com/dekchaigon/",
+  facebook: "https://www.facebook.com/Gon1811/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+            {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+            {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+            {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+            {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+            {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+                
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    role: "Portfolio by React",
+    date: "Oct 2024 - Present",
+    desc: "This mini-project involves designing and developing a portfolio website to comprehensively showcase your work and other important information. The website will feature a well-designed UX/UI to ensure it is visually appealing and easy to navigate.",
+    skills: [
+      "React",
+      "CSS",
+      "Git",
+      "Node JS",
+      "Vercel",
+      "JavaScript",
+      "XML",
+    ],
+  },
+  {
+    id: 1,
+    role: "Web Application",
+    date: "Aug 2023 - Nov 2023",
+    desc: "As part of a hands-on workshop in Web Application Development, I gained practical experience in full-stack web development.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Boostrap",
+      "JavaScript XML",
+      "PHP",
+      "JSON",
+      "Xampp",
+      "MySQL",
+      "phpMyAdmin",
+      "Apache Web Server",
+    ],
+    company: "https://github.com/ltzGon/webapp.git",
+  },
+  {
+    id: 2,
+    role: "Flutter Project",
+    date: "Dec 2023 - Mar 2023",
+    desc: "MOBILE & CLOUD COMPUTING.",
+    skills: [
+      "Flutter",
+      "Android Studio",
+      "Fire Base",
+      "Git",
+      "Dart",
+      "Docker",
+      "MySQL",
+      "PHP",
+      "React",
+    ],
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://yt3.googleusercontent.com/Dg-rzFmXzSAJN8ev-e1BCJPDYFn5Zyulb3z48DHg0TLDKMpqj9wuMmGcplYIrYd8Q97PfCH8JQ=s900-c-k-c0x00ffffff-no-rj",
+    school: "King Mongkut's University of Technology North Bangkok",
+    date:   "July 2021 - Present",
+    grade:  "2.24 GPAX",
+    desc:   "I am currently pursuing a Bachelor's degree in Computer Engineering at King Mongkut's University of Technology North Bangkok.",
+    degree: "Bachelor of Engineering - EnET(C), Electronics Engineering (Computer)",
+  },
+  {
+    id: 1,
+    img: "https://ptl.ac.th/wp-content/uploads/2021/05/FB_IMG_1620936967342-768x768.jpg",
+    school: "Phatthalung Technical College",
+    date:   "May 2019 - Apr 2021",
+    grade:  "3.58 GPAX",
+    desc:   "I have a background in computer from studying here. I have many good memories of friends. It was the best time in high school.",
+    degree: "Certificate of Vocational Education in Computer Technology",
+  },
+];
---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg:"#1C1C27",
+    bgLight: "#1C1E27",
+    primary:"#00e3ff",
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#00e3ff",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#1a2ddb",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#5c5b5b",
+}
